85375a895005563feda74e159c4e2523a175471a
AFrenchWrench <AFrenchWrench@gmail.com>
2025-05-18 23:43:38 +0330

    updated schema (changed field password_hash to password)

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS members (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
-  password_hash VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   joined_date DATE NOT NULL,
   role ENUM('member', 'admin') NOT NULL DEFAULT 'member'
 );
